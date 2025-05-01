16943bdcc77288bc6c8daa957821e8c1300d5469
kingshere <barnikchakraborty24@gmail.com>
2025-05-02 03:22:08 +0530

    Initial commit

---
+++ zerodha/example/simple.py
@@ -0,0 +1,35 @@
+import logging
+from kiteconnect import KiteConnect
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from modules import auth
+
+logging.basicConfig(level=logging.INFO)
+
+userdata = auth.get_userdata()
+kite = KiteConnect(api_key=userdata['api_key'])
+kite.set_access_token(userdata['access_token'])
+
+# Place an order
+
+try:
+    order_id = kite.place_order(
+        variety=kite.VARIETY_AMO,
+        exchange=kite.EXCHANGE_NSE,
+        tradingsymbol="SBIN",
+        transaction_type=kite.TRANSACTION_TYPE_BUY,
+        quantity=10,
+        product=kite.PRODUCT_CNC,
+        order_type=kite.ORDER_TYPE_MARKET
+    )
+
+    logging.info("Order placed. ID is: {}".format(order_id))
+except Exception as e:
+    logging.info("Order placement failed: {}".format(e.message))
+
+# Fetch all orders
+if __name__ == '__main__':
+    orders = kite.orders()
+    print(orders)
+
---
+++ zerodha/modules/auth.py
@@ -0,0 +1,42 @@
+from kiteconnect import KiteConnect
+import pandas as pd
+import os
+import sys
+import datetime
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+# Get the directory where this module is located
+module_dir = os.path.dirname(os.path.abspath(__file__))
+# Go up one level to get the zerodha directory
+zerodha_dir = os.path.dirname(module_dir)
+# Set the data path relative to the zerodha directory
+data_path = os.path.join(zerodha_dir, '../data')
+userdata_file = os.path.join(data_path, 'userdata.csv')
+
+def is_valid_token(api_key, access_token):
+    try:
+        kite = KiteConnect(api_key=api_key)
+        kite.set_access_token(access_token)
+        kite.profile()
+        return True
+    except Exception:
+        return False
+
+def get_userdata():
+    try:
+        if (os.path.exists(userdata_file)):
+            userdata = pd.read_csv(userdata_file)
+            api_key = userdata.loc[0, 'api_key']
+            access_token = userdata.loc[0, 'access_token']
+            user_id = userdata.loc[0, 'user_id']
+            public_token = userdata.loc[0, 'public_token']
+            if is_valid_token(api_key, access_token):
+                data = ({"api_key": api_key, "access_token": access_token, \
+                    "user_id": user_id, "public_token": public_token}) 
+                return data
+
+        raise Exception("** Run setup.py")
+    except Exception as e:
+        print("** ERROR in setup. ", e, datetime.datetime.now())
